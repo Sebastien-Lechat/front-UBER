49b3bb258d5614ee5caeaa59217786004658b3da
Sebastien-Lechat <seb_le@hotmail.fr>
2020-11-16 20:45:23 +0100

    fix class name

--- src/auth/register/register.js
+++ src/auth/register/register.js
@@ -10,30 +10,30 @@ const Register = () => {
                     <img src={logo2} alt="" className="title-img"/>
                 </div>
                 <div className="col-sm-12 px-0 center">
-                    <form id="register" className="login-form">
+                    <form id="register" className="register-form">
                         <div className="form-group">
-                            <label className="login-label">Nom</label>
+                            <label className="register-label">Nom</label>
                             <input type="text" className="form-control" id="name"  placeholder="Name"/>
                         </div>
                         <div className="form-group">
-                            <label className="login-label">Adresse Email</label>
+                            <label className="register-label">Adresse Email</label>
                             <input type="email" className="form-control" id="email"  placeholder="Email"/>
                         </div>
                         <div className="form-group">
-                            <label className="login-label">Téléphone</label>
+                            <label className="register-label">Téléphone</label>
                             <input type="text" className="form-control" id="phone"  placeholder="Téléphone"/>
                         </div>
                         <div className="form-group">
-                            <label className="login-label">Mot de passe :</label>
+                            <label className="register-label">Mot de passe :</label>
                             <input type="password" className="form-control" id="password"  placeholder="Mot de passe"/>
                         </div>
                         <div className="form-group">
-                            <label className="login-label">Confirmer le mot de passe :</label>
+                            <label className="register-label">Confirmer le mot de passe :</label>
                             <input type="password" className="form-control" id="confirmPassword"  placeholder="Confirmer le mot de passe"/>
                         </div>
-                        <button type="submit" className="btn login-btn">M'inscrire</button>
+                        <button type="submit" className="btn register-btn">M'inscrire</button>
                         <div className="col-sm-12 px-0 div-link d-flex justify-content-center">
-                            <span className="login-label">Vous avez déjà un compte ?&nbsp;<span className="link">Connexion</span></span>
+                            <span className="register-label">Vous avez déjà un compte ?&nbsp;<span className="link">Connexion</span></span>
                         </div>
                     </form>
                 </div>
