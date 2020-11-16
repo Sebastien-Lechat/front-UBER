7e18edce39816f9ad4cae5f87a1e39def9396301
EmilieRaza <razafiemilie@hotmail.fr>
2020-11-16 21:07:35 +0100

    update header mobile

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,68 +1,43 @@
-// import logo1 from './logo1.png';
-// import logo3 from './logo3.png';
-// import avatarTest from './avatarTest.png';
-// import logout from './logout.png';
+import logo1 from '../../assets/img/1.png';
+import logo3 from  '../../assets/img/uber2.png';
+import avatarTest from '../../assets/img/avatarTest2.png';
+import logout from '../../assets/img/16.png';
 import './header.css';
 
 function Header() {
   return (
-    <div className="App">
-<nav class="navbar navbar-expand-lg navbar-light white">
-  <div class="container"> 
 
-    <a class="navbar-brand" href="#">
-    
-    
-    </a> 
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
-            aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="basicExampleNav">
-
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          {/* <img src={logo3} alt="logo"/> */}
-        </li>
-        <li class="nav-item">
-          {/* <img src={logo1} alt="logo"/> */}
-         
-        </li>
-        <li class="nav-item" class="li-header-gauche3">
-          <a class="nav-link waves-effect">Dernière Connexion</a>
-        </li>
-      </ul>
-
-      
-      <ul class="navbar-nav nav-flex-icons">
-        <li class="nav-item">
-          <a href="https://www.facebook.com/mdbootstrap" class="nav-link waves-effect" target="_blank">
-            Mon historique
-          </a>
-        </li> 
-        <li class="nav-item">
-          <a href="https://twitter.com/MDBootstrap" class="nav-link waves-effect" target="_blank">
-           Mon Compte 
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="https://github.com/mdbootstrap/bootstrap-material-design" class="nav-link waves-effect">
-            Deconnexion
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link border border-light rounded waves-effect mr-2" target="_blank">
-            <i class="fas fa-envelope mr-1"></i>Logout
-            
-          </a>
-        </li>
-      </ul>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#" id="navbar-brand">
+    <span class="cercleBlanc"><br></br><img  src={avatarTest} alt="logo"/>  </span>
+     <img class="logoUberCercle"src={logo1} alt="logo"/> 
+     <img class="logoUber" src={logo3} alt="logo"/>  
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navDroite" aria-controls="navDroite" aria-expanded="true" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <img class="logoUberMobil" src={logo1} alt="logo"/>  
+  <div class="collapse navbar-collapse" id="derniereConnexion" >
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item text-center" id="VR"><a class="nav-link" href="#">DERNIÈRE CONNEXION<br></br>JJ/MM/AA</a></li>
+    </ul>
+  </div>
+  <div class="collapse navbar-collapse" id="navDroite" >
+  
+    <ul class="navbar-nav text-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HISTORIQUE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">MON COMPTE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">DECONNEXION &nbsp; <img class="imgLogout" src={logout} alt="Déconnexion"/></a>
+      </li>  
+    </ul>
   </div>
 </nav>
-    </div>
+
   );
 }
 
