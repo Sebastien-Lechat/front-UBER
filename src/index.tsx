9f4d6e1d8af97fa8d7744bd593fea9157f6e177c
Sebastien-Lechat <seb_le@hotmail.fr>
2020-11-18 09:55:15 +0100

    correct pwd-lost tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,53 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './component/header/header';
-import DoubleAuth from './auth/double-auth/double-auth';
-import Login from './auth/login/login';
-import Register from './auth/register/register';
-import PwdRecovery from './auth/pwd-recovery/pwd-recovery'
-import {ProtectedRoute, ProtectedRouteProps} from './guard/auth-gard';
-import {BrowserRouter as Router,Switch,Route,Link,useRouteMatch,useParams} from "react-router-dom";
+import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import VerifyEmail from './auth/verify-email/verify-email';
-import RequestPasswordRecovery from './auth/request-pwd-recovery/request-pwd-recovery';
-
-
-const defaultProtectedRouteProps: ProtectedRouteProps = {
-  isAuthenticated: false,
-  authenticationPath: '/login',
-};
 
 ReactDOM.render(
-    <Router>
-      <Switch>
-        <Route exact={true} path="/login">
-          <Login.Display />
-        </Route>
-        <Route exact={true} path="/register">
-          <Register.Display />
-        </Route>
-        <Route exact={true} path="/verify-email">
-          <VerifyEmail.Display />
-        </Route>
-        <Route exact={true} path="/double-auth">
-          <DoubleAuth.Display />
-        </Route>
-        <Route exact={true} path="/request-password-lost">
-          <RequestPasswordRecovery.Display />
-        </Route>
-        <Route exact={true} path="/double-auth">
-          <DoubleAuth.Display />
-        </Route>
-        <Route exact={true} path="/password-lost">
-          <PwdRecovery />
-        </Route>
-        <ProtectedRoute {...defaultProtectedRouteProps} exact={true} path='/' component={Header}/>
-        <Route path="*">
-          <Header />
-        </Route>
-      </Switch>
-    </Router>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
   document.getElementById('root')
   );
 
